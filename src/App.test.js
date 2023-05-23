94cfdfad1f8770992e8d2ac42b9e123016de1011
Payam Adineh <p.adineh@gmail.com>
2023-05-23 20:39:23 +0200

    initial frontend commit

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+
+test('renders learn react link', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div) ;
+});
